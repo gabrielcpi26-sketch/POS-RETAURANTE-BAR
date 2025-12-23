8a155fe3e58b48f9528d41a48d20174ee403c6b1
Gabriel <gabriel.cpi26@gmail.com>
2025-12-23 15:26:33 -0600

    axios

--- backend/src/api/axios.js
+++ backend/src/api/axios.js
@@ -1,8 +1,12 @@
+// src/api/axios.js
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:4000/api",
-});
+const baseURL =
+  import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "http://localhost:4000/api";
+
+const api = axios.create({ baseURL });
 
 // Interceptor para agregar token automáticamente
 api.interceptors.request.use((config) => {
@@ -12,3 +16,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
